d53047daec8a74e871ec1d2da4105299275f7643
naoise <G00385498@atu.ie>
2023-10-31 17:48:51 +0000

    Added second constructor that accepts a name then prompts user to fill in email and course

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -3,10 +3,17 @@ package ie.atu;
 public class Main {
     public static void main(String[] args) {
         Student student1 = new Student();
+        Student student2 = new Student("Naoise");
 
         student1.setStudentName();
         student1.setStudentEmail();
         student1.setStudentCourse();
+
+        student2.setStudentEmail();
+        student2.setStudentCourse();
+
         student1.displayInfo();
+        student2.displayInfo();
+
     }
 }
