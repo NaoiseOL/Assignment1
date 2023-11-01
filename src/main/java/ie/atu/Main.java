9696ef74a8208073f5a8da4e990154242cbadd32
naoise <G00385498@atu.ie>
2023-11-01 09:36:24 +0000

    Added constructor that accepts 3 parameters as well as added comments on the code

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,19 +1,36 @@
+//Naoise O'Loughlin
+//Group B
 package ie.atu;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        Student student2 = new Student("Naoise");
+        Student student1 = new Student();//created new student through a constructor in the student class
 
-        student1.setStudentName();
+        student1.setStudentName();//uses these three setters to set student information
         student1.setStudentEmail();
         student1.setStudentCourse();
 
-        student2.setStudentEmail();
+        Scanner scanner = new Scanner(System.in);//scanner createdu to intake name to be used in decleration of constructor
+        System.out.println("Please enter full name: ");
+        String name1 = scanner.nextLine();//scanner that intakes user input
+
+        Student student2 = new Student(name1);//created new student2 including the name taken in the scanner
+        student2.setStudentEmail();//uses setters to set rest of info through prompts
         student2.setStudentCourse();
 
-        student1.displayInfo();
+        System.out.println("Please enter full name: ");//using String based prompts with scanners to allow user to input information for student 3
+        String name2 = scanner.nextLine();
+        System.out.println("Please enter email: ");
+        String email = scanner.nextLine();
+        System.out.println("Please enter course: ");
+        String course = scanner.nextLine();
+
+        Student student3 = new Student(name2,email,course);//Student constructor created with all information given within above Strings and scanners
+
+
+        student1.displayInfo();//using a displayInfo function that consists of getters to show student information
         student2.displayInfo();
+        student3.displayInfo();
 
     }
 }
